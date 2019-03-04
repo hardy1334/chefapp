8e8e39b0a990510782e61da26d7009d828bdc261
hardy1334 <hardychugh34@gmail.com>
2019-03-04 22:15:59 +0530

    modified

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -31,6 +31,15 @@ class RecipesTest < ActionDispatch::IntegrationTest
 
    test "create new valid recipe" do
       get new_recipe_path
+      assert_template 'recipes/new'
+      name_of_recipe = "chicken saute"
+      description_of_recipe = "add some chicken,cooked vegetable for a meal"
+      assert_difference "Recipe.count", 1 do
+        post recipes_path,params:{recipe: {name: name_of_recipe,description: description_of_recipe}}
+      end
+      follow_redirect!
+      assert_match name_of_recipe.capitalize, response.body
+      assert_match description_of_recipe, response.body
    end
    test "reject invalid recipe submissions" do
       get new_recipe_path
